af8d6a5305753279fd22a5a4678053a8ae626f5a
Clint Wyatt <clintwyatt7@gmail.com>
2020-11-22 19:49:53 -0600

    Update dhcp_server.c

--- dhcp_server.c
+++ dhcp_server.c
@@ -12,99 +12,120 @@
 
 int main(int args, char **argv)
 {
-
+	//you type the network address as an IP address
 	if(args != 4)
 	{
 		printf("usage: ./server <portnumber> <network address> <subnet_part>\n");
 		return 0;
 	}
-	struct sockaddr_in servaddr, client;
+	struct sockaddr_in si_me, si_other;
 	unsigned int ipAddress, mask, *ipAddresses;
-	int port, recv_len, sendLen, maxfd, subnet_part, sockfd, size, index, *bitArray;
-	socklen_t cliLen;
-	char buffer[1024];
+	int port, recv_len, sendLen, maxfd, subnet_part, sockfd, size, index, *bitArray, cliLen;
 	port = atoi(argv[1]);
 	subnet_part = atoi(argv[3]);
 
+	//creating the socket
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	{
+		perror("socket");
+	}
+
+	memset((char *) &si_me, 0, sizeof(si_me));
+
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(port);
+	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	//bind socket to port
+	if(bind(sockfd, (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+	{
+		perror("bind");
+	}	
 	//setting up the ip addresses based on the subnet_port
 	mask = getMask(subnet_part);
+
+	int netAddress = getNetworkAddress(argv[2]);
+	netAddress = netAddress & mask;
+	//printf("%d \n", netAddress);
+
 	//setIP_addresses(ipAddresses, subnet_part);
 	unsigned int i, j, *arrSize;
 	size = getSize(subnet_part);
-	ipAddresses = (unsigned int *)malloc(sizeof(unsigned int) * size);
-	bitArray = (int *)malloc(sizeof(int) * size);
-	for(i =0xFFFFFF0, j =0; i < 0xFFFFFF0 + size; i++, j++)
+	ipAddresses = (unsigned int *)malloc(sizeof(unsigned int) * (size -2));
+	bitArray = (int *)malloc(sizeof(int) * (size -2));
+	for(i =netAddress +1, j =0; j < size-2; i++, j++)
 	{
 		ipAddresses[j] = i;
 	}
 	bzero(bitArray, size);//seroing out the bitArray
-	//printf("%d\n", size);
-	//printf("%d\n", arrSize[1]); 
 
-
-	//creating the socket
-	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
-	//setting up the server details
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(port);
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	//binding the socket
-	if(bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1)
-	{
-		perror("bind");
-		exit(EXIT_FAILURE);
-	}
-	//allocating memory for the packet
+	printf("%d \n", size);
 	struct dhcp_pkt *packet = (struct dhcp_pkt *) malloc(sizeof(struct dhcp_pkt));
-	cliLen = sizeof(client);
+	packet->yiaddr = netAddress;
+	cliLen = sizeof(si_other);
+	//printInfo(packet);
 	while(1)
 	{
 
-		if((recvfrom(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr *) &servaddr, &cliLen)) == -1)
+		if((recvfrom(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr *) &si_other, &cliLen)) == -1)
 		{
 			perror("recv_from");
 		}
 		else
 		{
+			printf("%s\n", packet->buffer);
 			if(strcmp(packet->buffer, "DHCPDISCOVER") == 0)//discover IP address
 			{
 				//find an available ip address
-				for(i =0; i < size; i++)
+				for(i =0; i < size-2; i++)
 				{
 					if(bitArray[i] == 0)
 					{
 						index = i;
+						bitArray[i] = 1;
 						break;
 					}
 				}
-				packet->yiaddr = ipAddresses[index];
-				packet->lifetime=3600;
-				bzero(packet->buffer, 1024);
-				memcpy(packet->buffer, "DHCPOFFER",9); 
-				if((sendto(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr*) &client, cliLen))== -1)
-				{
-					perror("send");
-				}
 
+				if(i == size -2)
+				{
+					printf("No more IP addressses are left\n");
+					memcpy(packet->buffer, "not available", sizeof("not available"));
+					if((sendto(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr*) &si_other, cliLen))== -1)
+					{
+						printf("send error: \n");
+						perror("send");
+					}
+
+				}
+				else
+				{
+
+					packet->yiaddr = ipAddresses[index];
+					packet->lifetime=3600;
+					bzero(packet->buffer, 1024);
+					memcpy(packet->buffer, "DHCPOFFER",9);
+					printInfo(packet);
+					printf("\n");
+					if((sendto(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr*) &si_other, cliLen))== -1)
+					{
+						printf("send error: \n");
+						perror("send");
+					}
+					bzero(packet->buffer, 1024);
+				}
 			}	
 			else if(strcmp(packet->buffer, "DHCPREQUEST") == 0) //request IP address
 			{
-				if((recvfrom(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr *) &servaddr, &cliLen)) == -1)
-				{
-					perror("recv_from");
-				}
 				bzero(packet->buffer, 1024);
 				memcpy(packet->buffer, "DHCPACK",7);
-                                if((sendto(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr*) &client, cliLen))== -1)
-                                {
-                                        perror("send");
-                                }	
+				printInfo(packet);
+				printf("\n");
+				if((sendto(sockfd, packet, sizeof(struct dhcp_pkt), 0, (struct sockaddr*) &si_other, cliLen))== -1)
+				{
+					printf("waahhh\n");	
+					perror("send");
+				}	
 			}
 
 		}
